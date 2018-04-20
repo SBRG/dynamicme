13f90de410935e7ce4f1e1d7ec6b559be4c277d7
Laurence <lyang@eng.ucsd.edu>
2018-04-19 20:42:57 -0700

    bookkeep all mets needed to grow vs. provided by source or crossfeed

--- dynamicme/location.py
+++ dynamicme/location.py
@@ -24,6 +24,217 @@ import pandas as pd
 import warnings
 
 
+
+class Locater(object):
+    """
+    Location model for microbiota given
+    nutrient sources, i, and occupance nodes ,j.
+    Each microbe (group) can also contribute to
+    availability of a nutrient via biosynthesis.
+    """
+    def __init__(self):
+        self.model = None
+        self.model_dict = {}
+
+    def create_problem(self, models, df_source, df_consumer, df_node, df_distance, df_organism):
+        """
+        Inputs
+        models : list of models
+        df_source
+            met  source  rxn  node  vmax
+        df_consumer
+            met  id (model.id)  rxn
+        df_node
+            node  x  y  type  Tf
+        df_distance
+            i  j  d
+        df_organism
+            id  X0 biomass (rxn)
+
+        max  sum_k sum_j ck' vkj
+        vkj,ykj,sijl,epjl
+        s.t. Sk vkj = 0                             (1)
+             lk*ykj <= vkj <= uk*ykj                (2)
+             sum_k ykj <= 1,  j in Nodes            (3)
+             -vkjl_up <= sum_i s_ijl/dij + sum_p e_pjl/dpj, (4)
+                k in Orgs, j in Nodes, l in Shared
+             sum_j e_pjl <= sum_k vkex_pl,          (5)
+                p in Nodes
+             sum_j s_ijl <= smax_il, i in Sources   (6)
+             sum_j ykj <= Xk0 + Xk0*sum_j mukj*Tfj, (7)
+                k in Orgs
+        """
+        model = Model('stacked')
+        # Only organism node locations
+        locs = df_node[df_node.type != 'source']['node'].unique()
+
+        # 1) Sk vkj = 0
+        for mdl in models:
+            for j in locs:
+                clone_id = '%s_%s'%(mdl.id, j)
+                mdlj = clone_model(mdl, model, suffix='_%s'%clone_id)
+                self.model_dict[clone_id] = mdlj
+        self.model = model
+
+        # 2) lk*ykj <= vkj <= uk*ykj
+        model_dict = self.model_dict
+        for mdl_id,mdl in iteritems(model_dict):
+            # mdl_id: modelk_nodej
+            ykj = Variable('y_%s'%mdl_id, lower_bound=0, upper_bound=1)
+            ykj.variable_kind = 'integer'
+            mdl.add_reaction(ykj)
+            for rxn in mdl.reactions:
+                cons_lb = Constraint('binary_lb_%s'%rxn.id)
+                cons_lb._bound = 0.
+                cons_lb._constraint_sense = 'L'
+                mdl.add_metabolites(cons_lb)
+                rxn.add_metabolites({cons_lb:-1.}, combine=False)
+                ykj.add_metabolites({cons_lb:rxn.lower_bound}, combine=False)
+                cons_ub = Constraint('binary_ub_%s'%rxn.id)
+                cons_ub._bound = 0.
+                cons_ub._constraint_sense = 'L'
+                mdl.add_metabolites(cons_ub)
+                rxn.add_metabolites({cons_ub:1.}, combine=False)
+                ykj.add_metabolites({cons_ub:-rxn.upper_bound}, combine=False)
+
+        # 3) sum_k ykj <= 1,  j in Nodes
+        organisms = [mdl.id for mdl in models]
+        for locj in locs:
+            cons = Constraint('sum_y_%s'%locj)
+            cons._bound = 1.
+            cons._constraint_sense = 'L'
+            model.add_metabolites(cons)
+            for org in organisms:
+                ykj = model.reactions.get_by_id('y_%s_%s'%(org,locj))
+                ykj.add_metabolites({cons:1.}, combine=False)
+
+        # 4) -vkjl_up <= sum_i s_ijl/dij + sum_p e_pjl/dpj
+        # 4) -vkjl_up - sum_i s_ijl/dij - sum_p e_pjl/dpj <= 0,
+        #       k in Orgs, j in Nodes, l in Shared
+        # svars = []  # Gather and add to model at the end
+        # evars = []
+        for ri,row in df_consumer.iterrows():
+            met  = row['met']
+            org  = row['id']
+            rid  = row['rxn']
+            for locj in locs:
+                cons = Constraint('uptake_cap_%s_%s_%s'%(org,locj,met))
+                cons._bound = 0.
+                cons._constraint_sense = 'L'
+
+                # Uptake rxn
+                mdl_id = '%s_%s'%(org,locj)
+                mdl = model_dict[mdl_id]
+                rxn_id = '%s_%s_%s'%(rid,org,locj)
+                ex_rxn = mdl.reactions.get_by_id(rxn_id)
+                ex_rxn.add_metabolites({cons:-1.}, combine=False)
+
+                # Primary sources
+                df_src_l = df_source[ df_source['met']==met]
+                df_primary = df_src_l[ df_src_l['source']=='primary']
+
+                for si,srow in df_primary.iterrows():
+                    loci = int(srow['node'])
+                    dij  = df_distance[
+                           (df_distance['i']==loci) & (df_distance['j']==locj)]['d'].iloc[0]
+                    sid = 's_%s_%s_%s'%(loci, locj, met)
+                    if model.reactions.has_id(sid):
+                        sijl = model.reactions.get_by_id(sid)
+                    else:
+                        sijl = Variable(sid)
+                        sijl.lower_bound = 0.
+                        sijl.upper_bound = 1000.
+                        model.add_reactions(sijl)
+                    #svars.append(sijl)
+                    sijl.add_metabolites({cons:-1./dij}, combine=False)
+
+                # Cross-feed sources
+                df_cross = df_src_l[ df_src_l['source']!='primary']
+                for si,srow in df_cross.iterrows():
+                    rid = srow['rxn']
+                    org_x = srow['source']
+                    for locp in locs:
+                        if locp != locj:
+                            dpj  = df_distance[
+                                (df_distance['i']==locp) & (df_distance['j']==locj)]['d'].iloc[0]
+                            eid ='e_%s_%s_%s'%(locp,locj,met)
+                            if model.reactions.has_id(eid):
+                                epjl = model.reactions.get_by_id(eid)
+                            else:
+                                epjl = Variable(eid)
+                                epjl.lower_bound = 0.
+                                epjl.upper_bound = 1000.
+                                model.add_reactions(epjl)
+                            # evars.append(epjl)
+                            epjl.add_metabolites({cons:-1./dpj}, combine=False)
+
+        # model.add_reactions(svars + evars)
+
+        # 5) sum_j e_pjl <= sum_k vkex_pl, p in Nodes, l in Shared
+        # 5) sum_j e_pjl - sum_k vkex_pl <= 0, p in Nodes, l in Shared
+        df_cross_feed = df_source[~df_source['rxn'].isnull()]
+        cross_mets = df_cross_feed['met'].unique()
+        for met in cross_mets:
+            df_cross = df_cross_feed[ df_cross_feed['met']==met]
+            for locp in locs:
+                cons = Constraint('crossfeed_capacity_%s_%s'%(locp, met))
+                cons._bound = 0.
+                cons._constraint_sense = 'L'
+                model.add_metabolites(cons)
+                for ir,row in df_cross.iterrows():
+                    org = row['source']
+                    rid = row['rxn']    # Exchange flux providing this met
+                    # All orgs at p providing l
+                    mdl_p_id = '%s_%s'%(org,locp)
+                    mdl_p = model_dict[mdl_p_id]
+                    rxn_id = '%s_%s_%s'%(rid,org,locp)
+                    rxn = mdl_p.reactions.get_by_id(rxn_id)
+                    rxn.add_metabolites({cons:-1.}, combine=False)
+                # e_pjl: only cross-feed supplied
+                for locj in locs:
+                    if locj != locp:
+                        e_id = 'e_%s_%s_%s'%(locp,locj,met)
+                        e_pjl = model.reactions.get_by_id(e_id)
+                        e_pjl.add_metabolites({cons:1.}, combine=False)
+
+        # 6) sum_j s_ijl <= smax_il, i in Sources, l in Shared
+        df_primary_feed = df_source[df_source['source']=='primary']
+        primary_mets = df_primary_feed['met'].unique()
+        for met in primary_mets:
+            df_primary = df_primary_feed[ df_primary_feed['met']==met]
+            for ir,row in df_primary.iterrows():
+                loci = int(row['node'])
+                vmax = row['vmax']
+                cons = Constraint('source_capacity_%s_%s'%(loci,met))
+                cons._bound = vmax
+                cons._constraint_sense = 'L'
+                model.add_metabolites(cons)
+                for locj in locs:
+                    s_id  = 's_%s_%s_%s'%(loci,locj,met)
+                    s_ijl = model.reactions.get_by_id(s_id)
+                    s_ijl.add_metabolites({cons:1.}, combine=False)
+
+        # 7) sum_j ykj <= Xk0 + Xk0*sum_j mukj*Tfj
+        # 7) sum_j ykj - Xk0*sum_j mukj*Tfj <= Xk0
+        for oi,row in df_organism.iterrows():
+            X0 = row['X0']
+            org = row['id']
+            mu_id = row['biomass']
+            cons = Constraint('colonization_%s'%org)
+            cons._bound = X0
+            cons._constraint_sense = 'L'
+            model.add_metabolites(cons)
+            for locj in locs:
+                Tf = df_node.loc[locj]['Tf']    # flow period is location-dependent
+                rxn_id = '%s_%s_%s'%(mu_id,org,locj)
+                rxn_mu = model.reactions.get_by_id(rxn_id)
+                rxn_mu.add_metabolites({cons:-X0*Tf}, combine=False)
+                yid = 'y_%s_%s'%(org,locj)
+                ykj = model.reactions.get_by_id(yid)
+                ykj.add_metabolites({cons:1.}, combine=False)
+
+
+
 class LocateMM(object):
     """
     Location model for microbiota given
